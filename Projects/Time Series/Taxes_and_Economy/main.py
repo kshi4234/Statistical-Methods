24e3e9d6b540341fa6b10bbfc9780de9e59f17a2
kshi4234 <kshi4234@gmail.com>
2025-05-21 02:19:37 -0400

    data loading utilities and some preliminary data manipulation

--- Projects/Time Series/Taxes_and_Economy/main.py
+++ Projects/Time Series/Taxes_and_Economy/main.py
@@ -1,2 +1,49 @@
 import numpy as np
+from utils import data_loading
 
+# Impute missing values in the dataset using regression, as there is not a lot of it.
+def impute(dataset, variables, target):
+    return
+
+def clean(dataset, variables, target):
+    """ Some notes on what to clean.
+    1) Column 2024. GDP growth data only goes to 2023, not sure why it's included
+    2) Call impute to impute the missing values 
+    """
+    return
+
+# Do some exploratory analysis of the data. 
+def data_exploration(dataset, variables, target):
+    """
+    For my purposes, I will simply plot the United States data as a function of time.
+    This will serve to help me decide what types of modeling would be suited for my
+    dataset.
+    
+    Tax data is quite complicated, as oftentimes the tax rates change, to say nothing of
+    how the number of brackets change as well. 
+    """
+    gdp_data = dataset[target[1]]
+    health_data = dataset[variables[1]]
+    milex_data = dataset[variables[2]]
+    # TODO: Tax data I want to separate into 2 different variables, where they will 
+    # TODO: be split into single filer tax brackets. One will be the highest tax bracket,
+    # TODO: the other will be the lowest.
+    tax_data = dataset[variables[3]]
+    us_gdp = gdp_data[gdp_data['Country Code']=='USA']
+    us_health = health_data[health_data['Expenditure Amount (Millions)']=='Total National Health Expenditures']
+    us_milex = milex_data[milex_data['Country Code']=='USA']
+    
+    print(us_gdp)
+    print(us_health)
+    print(us_milex)
+    
+
+def main():
+    dataset, variables, target = data_loading.load_dataset()
+    print(variables)
+    print(target)
+    print(dataset[variables[1]])
+    data_exploration(dataset, variables, target)
+    
+if __name__ == '__main__':
+    main()
--- Projects/Time Series/Taxes_and_Economy/utils/data_loading.py
+++ Projects/Time Series/Taxes_and_Economy/utils/data_loading.py
@@ -2,13 +2,34 @@ import numpy as np
 import pandas as pd
 import os
 
-print(os.getcwd())
-
-def load_data(filename, header=0):
-    df = pd.read_csv(filename, header=header)
+def load_data(filename, header=0, type='csv', encoding='utf-8'):
+    df = ''
+    if type == 'csv':
+        df = pd.read_csv(filename, header=header, encoding=encoding)
     return df
 
+def load_dataset():
+    # Gross Domestic Product
+    gdp_file = "E:/...DATA SCIENCE AND QUANT RESOURCES/Classical-Statistical-Methods/Projects/Time Series/Taxes_and_Economy/data/GDP_Growth.csv"
+    # National Health Expenditures   
+    nhe_file = "E:/...DATA SCIENCE AND QUANT RESOURCES/Classical-Statistical-Methods/Projects/Time Series/Taxes_and_Economy/data/NHE2023.csv"
+    # Military Expenditures  
+    milex_file = "E:/...DATA SCIENCE AND QUANT RESOURCES/Classical-Statistical-Methods/Projects/Time Series/Taxes_and_Economy/data/API_MS.MIL.XPND.CD_DS2_en_csv_v2_85458.csv"  
+    # Tax files
+    tax_file = "E:/...DATA SCIENCE AND QUANT RESOURCES/Classical-Statistical-Methods/Projects/Time Series/Taxes_and_Economy/data/Historical Income Tax Rates and Brackets, 1862-2021.csv" 
+    
+    variables = ['explanatory variables:', 'nhe_data','milex_data','tax_data']
+    variable_files = ['explanatory variables:', (nhe_file, 1), (milex_file, 2), (tax_file, 0)]
+    target = ['response/target:', 'gdp_data']
 
-gdp_file = "E:\...DATA SCIENCE AND QUANT RESOURCES\Classical-Statistical-Methods\Projects\Time Series\Taxes_and_Economy\data\GDP_Growth.csv"
-df_gdp = load_data(gdp_file, header=2)  # Pandas dataframe object
-print(df_gdp)
+    dataset = {}
+    
+    # Pandas dataframe objects
+    df_gdp = load_data(gdp_file, header=2, encoding="unicode_escape")
+    dataset[target[1]] = df_gdp
+    
+    for i in range(1, len(variables)):
+        # print(variables[i])
+        dataset[variables[i]] = load_data(variable_files[i][0], header=variable_files[i][1], encoding="unicode_escape")
+    return dataset, variables, target
+    
